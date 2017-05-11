f92364030d54e0bc36afbdba8aa20f6c62abe081
shinichi fukuda <s-fukuda@iij.ad.jp>
2017-05-11 17:49:10 +0900

    update:   nginxbuild.sh

--- nginxbuild.sh
+++ nginxbuild.sh
@@ -9,15 +9,15 @@ if [ -z "$NGINX_VERSION" -a -z "$OPENSSL_VERSION" ]; then
   exit 1
 fi
 
-# cd $HOME/rpmbuild/SOURCES && curl -LO http://cache.ruby-lang.org/pub/ruby/$(echo $RUBY_VERSION | sed -e 's/\.[0-9]$//')/ruby-$RUBY_VERSION.tar.gz
+ls ./$V_FILES && \
+cp $V_FILES $HOME/rpmbuild/SOURCES 
+
 cd $HOME/rpmbuild/SOURCES && curl -LO http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz && \
 curl -LO https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz && \
 git clone https://github.com/SpiderLabs/ModSecurity.git mod_security && \
 cd mod_security && ./autogen.sh && ./configure --enable-standalone-module ; make
 
 cd $HOME
-echo $V_FILES
-cp $V_FILES $HOME/rpmbuild/SOURCES && \
 rpmbuild -ba $HOME/rpmbuild/SPECS/nginx1x.spec
 
 cp $HOME/rpmbuild/RPMS/x86_64/* /shared/
