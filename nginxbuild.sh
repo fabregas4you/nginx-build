b0b55a563dba0b7c69b4c26c567a29540d32d87f
shinichi fukuda <s-fukuda@iij.ad.jp>
2017-05-11 15:51:35 +0900

    wait for git clone

--- nginxbuild.sh
+++ nginxbuild.sh
@@ -10,12 +10,11 @@ fi
 
 # cd $HOME/rpmbuild/SOURCES && curl -LO http://cache.ruby-lang.org/pub/ruby/$(echo $RUBY_VERSION | sed -e 's/\.[0-9]$//')/ruby-$RUBY_VERSION.tar.gz
 cd $HOME/rpmbuild/SOURCES && curl -LO http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz && \
-curl -LO https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
-which git && \
-git clone https://github.com/SpiderLabs/ModSecurity.git mod_security
-
+curl -LO https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz && \
+git clone https://github.com/SpiderLabs/ModSecurity.git mod_security && \
 cd mod_security && ./autogen.sh && ./configure --enable-standalone-module ; make
 
+cp nginx-version openssl-version $HOME/rpmbuild/SOURCES && \
 rpmbuild -ba $HOME/rpmbuild/SPECS/nginx1x.spec
 
 cp $HOME/rpmbuild/RPMS/x86_64/* /shared/
