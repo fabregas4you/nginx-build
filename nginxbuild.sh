dcc75e8d937f37b98984ecf14b73f543c6621bd7
shinichi fukuda <s-fukuda@iij.ad.jp>
2017-05-11 18:14:12 +0900

    pdate:   dockerfile-6.8, nginxbuild.sh

--- nginxbuild.sh
+++ nginxbuild.sh
@@ -10,16 +10,13 @@ if [ -z "$NGINX_VERSION" -a -z "$OPENSSL_VERSION" ]; then
 fi
 
 # ls ./$V_FILES && \
-ls . && \
-cp $V_FILES $HOME/rpmbuild/SOURCES 
 
 cd $HOME/rpmbuild/SOURCES && curl -LO http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz && \
 curl -LO https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz && \
 git clone https://github.com/SpiderLabs/ModSecurity.git mod_security && \
 cd mod_security && ./autogen.sh && ./configure --enable-standalone-module ; make
 
-cd $HOME
 rpmbuild -ba $HOME/rpmbuild/SPECS/nginx1x.spec
 
 cp $HOME/rpmbuild/RPMS/x86_64/* /shared/
-# cp $HOME/rpmbuild/SRPMS/* /shared/
+cp $HOME/rpmbuild/SRPMS/* /shared/
