f218344ee1f997c5fbe16e57c4fe47305c948a6c
shinichi fukuda <s-fukuda@iij.ad.jp>
2017-05-11 17:27:12 +0900

    update: nginxbuild.sh

--- nginxbuild.sh
+++ nginxbuild.sh
@@ -2,6 +2,7 @@
 HOME=/var/tmp
 NGINX_VERSION=$(cat nginx-version)
 OPENSSL_VERSION=$(cat openssl-version)
+V_FILES=`ls ./*-versions`
 
 if [ -z "$NGINX_VERSION" -a -z "$OPENSSL_VERSION" ]; then
   echo "required nginx-version and openssl-version file."
@@ -14,9 +15,10 @@ curl -LO https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz && \
 git clone https://github.com/SpiderLabs/ModSecurity.git mod_security && \
 cd mod_security && ./autogen.sh && ./configure --enable-standalone-module ; make
 
-cd $HOME/rpmbuild/
-cp nginx-version openssl-version $HOME/rpmbuild/SOURCES && \
+cd $HOME
+echo $V_FILES
+cp $V_FILES $HOME/rpmbuild/SOURCES && \
 rpmbuild -ba $HOME/rpmbuild/SPECS/nginx1x.spec
 
 cp $HOME/rpmbuild/RPMS/x86_64/* /shared/
-cp $HOME/rpmbuild/SRPMS/* /shared/
+# cp $HOME/rpmbuild/SRPMS/* /shared/
